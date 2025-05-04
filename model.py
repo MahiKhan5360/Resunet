8fa7bbcc4e91939346cad37d6f6374f5f9a191b8
MahiKhan5360 <86805135+MahiKhan5360@users.noreply.github.com>
2025-05-04 19:55:28 +0600

    Update model.py

--- model.py
+++ model.py
@@ -35,5 +35,30 @@ class ResUnet(nn.Module):
             nn.Conv2d(filters[0], 1, 1, 1),
             nn.Sigmoid(),
         )
+  def forward(self, x):
+        # Encode
+        x1 = self.input_layer(x) + self.input_skip(x)
+        x2 = self.residual_conv_1(x1)
+        x3 = self.residual_conv_2(x2)
+        # Bridge
+        x4 = self.bridge(x3)
+        # Decode
+        x4 = self.upsample_1(x4)
+        x5 = torch.cat([x4, x3], dim=1)
 
+        x6 = self.up_residual_conv1(x5)
+
+        x6 = self.upsample_2(x6)
+        x7 = torch.cat([x6, x2], dim=1)
+
+        x8 = self.up_residual_conv2(x7)
+
+        x8 = self.upsample_3(x8)
+        x9 = torch.cat([x8, x1], dim=1)
+
+        x10 = self.up_residual_conv3(x9)
+
+        output = self.output_layer(x10)
+
+        return output
   
