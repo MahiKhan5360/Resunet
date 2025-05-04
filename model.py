8dca08f354f88b4eb2443137829c61a846273753
MahiKhan5360 <86805135+MahiKhan5360@users.noreply.github.com>
2025-05-04 19:53:54 +0600

    Create model.py

---
+++ model.py
@@ -0,0 +1,16 @@
+import torch
+import torch.nn as nn
+from core.modules import ResidualConv, Upsample
+
+
+class ResUnet(nn.Module):
+    def __init__(self, channel, filters=[64, 128, 256, 512]):
+        super(ResUnet, self).__init__()
+
+        self.input_layer = nn.Sequential(
+            nn.Conv2d(channel, filters[0], kernel_size=3, padding=1),
+            nn.BatchNorm2d(filters[0]),
+            nn.ReLU(),
+            nn.Conv2d(filters[0], filters[0], kernel_size=3, padding=1),
+        )
+       
