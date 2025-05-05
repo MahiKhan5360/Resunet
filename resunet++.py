79f0395189d31fd3fd453907b595b6752dae47a8
MahiKhan5360 <86805135+MahiKhan5360@users.noreply.github.com>
2025-05-05 10:04:27 +0600

    Create resunet++.py

---
+++ resunet++.py
@@ -0,0 +1,56 @@
+import torch.nn as nn
+import torch
+from core.modules import (
+    ResidualConv,
+    ASPP,
+    AttentionBlock,
+    Upsample_,
+    Squeeze_Excite_Block,
+)
+
+
+class ResUnetPlusPlus(nn.Module):
+    def __init__(self, channel, filters=[32, 64, 128, 256, 512]):
+        super(ResUnetPlusPlus, self).__init__()
+
+        self.input_layer = nn.Sequential(
+            nn.Conv2d(channel, filters[0], kernel_size=3, padding=1),
+            nn.BatchNorm2d(filters[0]),
+            nn.ReLU(),
+            nn.Conv2d(filters[0], filters[0], kernel_size=3, padding=1),
+        )
+        self.input_skip = nn.Sequential(
+            nn.Conv2d(channel, filters[0], kernel_size=3, padding=1)
+        )
+
+        self.squeeze_excite1 = Squeeze_Excite_Block(filters[0])
+
+        self.residual_conv1 = ResidualConv(filters[0], filters[1], 2, 1)
+
+        self.squeeze_excite2 = Squeeze_Excite_Block(filters[1])
+
+        self.residual_conv2 = ResidualConv(filters[1], filters[2], 2, 1)
+
+        self.squeeze_excite3 = Squeeze_Excite_Block(filters[2])
+
+        self.residual_conv3 = ResidualConv(filters[2], filters[3], 2, 1)
+
+        self.aspp_bridge = ASPP(filters[3], filters[4])
+
+        self.attn1 = AttentionBlock(filters[2], filters[4], filters[4])
+        self.upsample1 = Upsample_(2)
+        self.up_residual_conv1 = ResidualConv(filters[4] + filters[2], filters[3], 1, 1)
+
+        self.attn2 = AttentionBlock(filters[1], filters[3], filters[3])
+        self.upsample2 = Upsample_(2)
+        self.up_residual_conv2 = ResidualConv(filters[3] + filters[1], filters[2], 1, 1)
+
+        self.attn3 = AttentionBlock(filters[0], filters[2], filters[2])
+        self.upsample3 = Upsample_(2)
+        self.up_residual_conv3 = ResidualConv(filters[2] + filters[0], filters[1], 1, 1)
+
+        self.aspp_out = ASPP(filters[1], filters[0])
+
+        self.output_layer = nn.Sequential(nn.Conv2d(filters[0], 1, 1), nn.Sigmoid())
+
+    
